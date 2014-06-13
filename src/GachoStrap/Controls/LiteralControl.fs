4355c7bcc5c67d59ebd018de1f9ea230bcb2108d
Micha-kun <michakun@gmail.com>
2014-06-13 22:50:59 +0200

    Nueva solución y proyecto de F#. LiteralControl

---
+++ src/GachoStrap/Controls/LiteralControl.fs
@@ -0,0 +1,13 @@
+﻿namespace GachoStrap.Controls
+
+open System.Web.UI
+
+type LiteralControl() =
+    inherit Control()
+    let mutable text: string = null 
+    member this.Text 
+        with public get() = text
+        and public set value = text <- value
+    override this.Render(writer : HtmlTextWriter) =
+        writer.Write(this.Text)
+            
