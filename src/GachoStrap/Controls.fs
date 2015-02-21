1d4f2c7a58befd61f8872a7998c5c96c24ded6ba
Michakun Gómez <michakun@gmail.com>
2015-02-21 22:39:04 +0100

    Project Reset

--- src/GachoStrap/Controls.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace GachoStrap.Controls
-
-open System.Web.UI
-
-type LiteralControl(text: string) =
-    inherit Control()
-    member val Text = text with get, set 
-    override this.Render(writer) =
-        writer.Write(this.Text)
