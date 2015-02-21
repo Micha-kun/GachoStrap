0f7d3f905a53bab769d52cde42cc2b06d40271c5
Michakun Gómez <michakun@gmail.com>
2015-02-21 23:11:10 +0100

    Simplistic controls

--- src/GachoStrap/Controls.fs
+++ src/GachoStrap/Controls.fs
@@ -2,5 +2,38 @@
 
 open System.Web.UI
 
-type Class1() = 
-    member this.X = "F#"
+type TextBox() =
+    inherit Control() 
+    override this.Render writer =
+        writer.Write "TextBox"
+        base.Render writer
+
+type Button() =
+    inherit Control() 
+    override this.Render writer =
+        writer.Write "Button"
+        base.Render writer
+
+type HyperLink() =
+    inherit Control() 
+    override this.Render writer =
+        writer.Write "HyperLink"
+        base.Render writer
+
+type CheckBox() =
+    inherit Control() 
+    override this.Render writer =
+        writer.Write "CheckBox"
+        base.Render writer
+
+type RadioButton() =
+    inherit Control() 
+    override this.Render writer =
+        writer.Write "RadioButton"
+        base.Render writer
+
+type HiddenField() =
+    inherit Control() 
+    override this.Render writer =
+        writer.Write "HiddenField"
+        base.Render writer
