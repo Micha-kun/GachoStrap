67cd4aaef4586672575fef3238a738ce4a7fc1f1
Michael-Jorge Gómez <michakun@gmail.com>
2015-03-09 15:48:36 +0100

    Usage of EventHandlerList

--- src/GachoStrap/Controls.fs
+++ src/GachoStrap/Controls.fs
@@ -10,17 +10,34 @@ type TextChangedEventArgs(oldValue, newValue) =
 
 type TextBox() =
     inherit Control()
-    let textChanged = new Event<EventHandler<TextChangedEventArgs>, TextChangedEventArgs>()
+
+    static let textChangedKey = new obj();
+
+    member private this.textChangedAddHandler hdl = base.Events.AddHandler (textChangedKey, hdl)
+    member private this.textChangedRemoveHandler hdl = base.Events.RemoveHandler (textChangedKey, hdl)
+
+    member private this.OnTextChanged args = 
+        match base.Events.[textChangedKey] with
+            | :? EventHandler<TextChangedEventArgs> as hdl -> hdl.Invoke(this, args)
+            | _ -> ignore()
 
     [<CLIEvent>]
-    member this.TextChanged = textChanged.Publish
+    member this.TextChanged = 
+        { new IDelegateEvent<EventHandler<TextChangedEventArgs>> with
+              member x.AddHandler(handler) = 
+                  this.textChangedAddHandler handler
+              
+              member x.RemoveHandler(handler) = 
+                  this.textChangedRemoveHandler handler }
+
 
     member this.Text 
         with get () = this.ViewState.["Text"] :?> string
         and set (value : string) = 
             let oldValue = this.Text
             this.ViewState.["Text"] <- value
-            textChanged.Trigger (this, new TextChangedEventArgs(oldValue, value))
+            this.OnTextChanged (new TextChangedEventArgs(oldValue, value))
+
     override this.Render writer =
         writer.Write (@"<input type=""text"" value=""" + this.Text + @""" />")
         base.Render writer
